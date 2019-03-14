ab884224881123f4b0338ec4dfe064c553e2c2c2
Shirley Gonzalez <shirleypamela1020@gmail.com>
2019-03-14 22:23:01 +0000

    First Day

--- js/script.js
+++ js/script.js
@@ -5,7 +5,15 @@
 
 $("#search-button").click(function(){
   
-  
+  $.ajax({
+        url:'https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC',
+        method: "GET",
+        success: function(response) {
+            console.log(response.data[0]);
+           
+           
+        }   
+    });         
   
 });
 
