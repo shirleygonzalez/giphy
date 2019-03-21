df8cc9bdbc774009e6a1c07b5eff3c4276b36b1a
shirleygonzalez <shirleygonzalez@aclanyc.com>
2019-03-21 22:22:38 +0000

    second day done

--- js/script.js
+++ js/script.js
@@ -4,16 +4,21 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
+
+    var searchInput = $("input").val();
+
+    console.log(searchInput);
+
+
   $.ajax({
-        url:'https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC',
+        url:'https://api.giphy.com/v1/gifs/search?q='+searchInput+'&rating=pg&api_key=dc6zaTOxFJmzC',
         method: "GET",
         success: function(response) {
-            console.log(response.data[0]);
-           
-           
-        }   
-    });         
-  
+            var imageUrl= response.data[1].images.fixed_width.url;
+            console.log(imageUrl);
+             $('body').append('<img src='+imageUrl+'/>');
+        }
+    });
+
 });
 
